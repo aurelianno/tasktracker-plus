89df643f389da5fa4621bcc942b8561d4979841b
aurelianno <ceballos4@outlook.com>
2025-07-11 15:03:11 -0700

    Add dayjs dependency and update .gitignore for environment variables

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/tasktracker">
           <App />
           <Toaster position="top-right" />
         </BrowserRouter>
